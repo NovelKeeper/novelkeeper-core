439ad2fcb3c7dfc29f4f0e87bb7603aa227993b0
John Ramberger <rambergerjohn@gmail.com>
2023-07-29 22:47:44 -0400

    Update novelfull_com source

--- src/model/source/impl/novelfull_com.ts
+++ src/model/source/impl/novelfull_com.ts
@@ -1,6 +1,9 @@
+import { parse } from 'node-html-parser';
+
 import { NotImplementedException } from '../../../exception/exception';
 import { NovelMetadata } from '../../novel/novel_metadata';
 import { NKSource } from '../source';
+import { NKUrl } from '../../../util/url';
 
 export class NovelFullCom extends NKSource {
   id = 1;
@@ -8,9 +11,55 @@ export class NovelFullCom extends NKSource {
   hostname = 'novelfull.com';
   chapterListPaginated = true;
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async extractNovelMetadata(_html: string): Promise<NovelMetadata> {
-    throw new NotImplementedException('NovelFullCom.extractChapterUrls()');
+  async extractNovelMetadata(
+    _url: NKUrl,
+    _html: string
+  ): Promise<NovelMetadata> {
+    const root = parse(_html);
+    const title =
+      root
+        .querySelector('div.col-xs-12:nth-child(3) > h3:nth-child(1)')
+        ?.text.trim() ?? 'Unknown Title';
+
+    const authors = root
+      .querySelectorAll('.info > div:nth-child(1) > a')
+      .map((a) => a.text.trim());
+
+    const description = root
+      .querySelectorAll('.desc-text > p')
+      .map((p) => p.text.trim());
+
+    const genres = root
+      .querySelectorAll('.info > div:nth-child(3) > a')
+      .map((a) => a.text.trim());
+
+    const coverUrl = new NKUrl(
+      root.querySelector('.book > img:nth-child(1)')?.getAttribute('src') ?? ''
+    );
+
+    // Alternate titles <>Alternative names: <h3></h3> title1, title2</>
+    const altTitles =
+      root
+        .querySelector('.info > div:nth-child(2)')
+        ?.text.trim()
+        .split('names:')[1]
+        .split(',')
+        .map((title) => title.trim()) ?? [];
+
+    const status =
+      root.querySelector('.info > div:nth-child(5) > a:nth-child(2)')?.text ??
+      '';
+
+    return new NovelMetadata(
+      _url,
+      title,
+      authors,
+      description,
+      genres,
+      coverUrl,
+      status,
+      altTitles
+    );
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
--- src/model/source/source.ts
+++ src/model/source/source.ts
@@ -1,3 +1,4 @@
+import { NKUrl } from '../../util/url';
 import { NovelMetadata } from '../novel/novel_metadata';
 
 export abstract class NKSource {
@@ -24,10 +25,14 @@ export abstract class NKSource {
 
   /**
    * Extract the novel metadata from the novel page html
+   * @param _url The url of the novel page
    * @param _html The html of the novel page
    * @returns A promise that resolves to a NovelMetadata object
    */
-  abstract extractNovelMetadata(_html: string): Promise<NovelMetadata>;
+  abstract extractNovelMetadata(
+    _url: NKUrl,
+    _html: string
+  ): Promise<NovelMetadata>;
 
   /**
    * Extract the chapter urls from the novel page html
